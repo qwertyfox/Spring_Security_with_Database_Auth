733c6cd98eac0e3a5a97a4c71c167c01578b1efa
qwertyfox <67963327+qwertyfox@users.noreply.github.com>
2020-10-23 11:14:01 +1100

    Loading the project to the Repo

---
+++ src/main/java/com/qwertyfox/encrypted_password/SpringSecurityWithDatabaseAuthApplication.java
@@ -0,0 +1,13 @@
+package com.qwertyfox.encrypted_password;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityWithDatabaseAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityWithDatabaseAuthApplication.class, args);
+    }
+
+}
