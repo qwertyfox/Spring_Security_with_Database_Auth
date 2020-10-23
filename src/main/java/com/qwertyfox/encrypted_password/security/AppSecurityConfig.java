cb0ccdb58fc5e30cc4e8b1382d6f781c3783f505
qwertyfox <67963327+qwertyfox@users.noreply.github.com>
2020-10-23 12:06:19 +1100

    * AppSecurityConfig class coded with link to Service and password encoder. * Set the authentication provider to DaoAuthenticationProvider and with custom userDetailsService

--- src/main/java/com/qwertyfox/encrypted_password/security/AppSecurityConfig.java
+++ src/main/java/com/qwertyfox/encrypted_password/security/AppSecurityConfig.java
@@ -1,13 +1,36 @@
 package com.qwertyfox.encrypted_password.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @Configuration
 @EnableWebSecurity
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+    private final UserDetailsService userDetailsService;
+
+    @Autowired
+    public AppSecurityConfig(PasswordEncoder passwordEncoder, @Qualifier("myUDS") UserDetailsService userDetailsService) {
+        this.passwordEncoder = passwordEncoder;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder);
+        provider.setUserDetailsService(userDetailsService);
+        return provider;
+    }
 
 
 }
